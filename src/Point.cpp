623312663a3a8862d1d89aff9101156d3f6fee38
chen15959 <chen15959@163.com>
2019-01-24 17:40:34 +0800

    add Point

---
+++ src/Point.cpp
@@ -0,0 +1,63 @@
+#include "Point.hpp"
+
+//#include "Board.hpp"
+
+
+
+
+Point::Point(int row, int col, Board * board)
+{
+	this->_row = row;
+	this->_col = col;
+
+	this->_value = VAL_UNKNOWN;
+	this->_round = -1;
+	
+	this->_board = board;
+}
+
+
+
+Point::Point(const Point & other)
+{
+	this->_row = other._row;
+	this->_col = other._col;
+	this->_value = other._value;
+	this->_round = other._round;
+	this->_board = other._board;
+}
+
+
+
+Point::~Point()
+{
+	// do nothing
+}
+
+
+
+Point & Point::operator=(const Point & rhs)
+{
+	if (&rhs != this)
+	{
+		this->_row = rhs._row;
+		this->_col = rhs._col;
+		this->_value = rhs._value;
+		this->_round = rhs._round;
+		this->_board = rhs._board;
+	}
+	
+	return *this;
+}
+
+
+//设置Point的值
+//和Board联动
+int Point::setValue(char value)
+{
+	this->_value = value;
+	
+	//todo
+	return 0;
+}
+	
---
+++ src/Point.hpp
@@ -0,0 +1,100 @@
+/*
+棋盘上的一个点
+	解谜场景：
+	初始化的时候每个点都是不确定的（?）
+	解完后每个点都有一个确定的值（A ~ Z）(单色模式下，只有A值)
+	或者空（.）
+	每个点都记录着自己被解出的信息
+*/
+#ifndef Point_h
+#define Point_h
+
+class Board;
+
+//未解谜
+#define VAL_UNKNOWN	 '?'
+//已解谜，是空
+#define VAL_EMPTY	 '.'
+
+
+//点类
+class Point
+{
+public:
+	//构造 带自己的位置
+	Point(int row, int col, Board * board);
+	
+	//拷贝构造
+	Point(const Point & other);
+	//析构
+	~Point();
+	
+	//赋值运算符重载
+	Point & operator=(const Point & rhs);
+	
+public:
+	//点在哪行（0开始）
+	inline
+	int getRow() const {
+		return this->_row;
+	}
+	
+	//点在哪列（0开始）
+	inline
+	int getCol() const {
+		return this->_col;
+	}
+	
+	//获得点的值
+	inline
+	char getValue() const {
+		return this->_value;
+	}
+	
+	//设置点为空
+	inline
+	int setEmpty() {
+		return setValue(VAL_EMPTY);
+	}
+	
+	//设置点的值
+	int setValue(char value);
+	
+	
+	//获得解谜索引
+	inline
+	int getRound() const {
+		return this->_round;
+	}
+	
+	//设置解谜索引
+	inline
+	void setRound(int v) {
+		this->_round = v;
+	}
+	
+	//该点是否已经确定
+	inline
+	bool isDone() const {
+		return (_value != VAL_UNKNOWN);
+	}
+	
+	
+	
+private:
+	//行（0开始）
+	int			_row;
+	//列（0开始）
+	int			_col;
+	//值
+	char		_value;
+	//解谜索引
+	int			_round;
+	//所属的board
+	Board *		_board;
+	
+};
+
+
+#endif
+
