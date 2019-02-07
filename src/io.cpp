07b0fac96305fd8f121bbaa021e2f46fd8597045
chen15959 <chen15959@163.com>
2019-02-07 22:28:00 +0800

    use param_handler

--- src/io.cpp
+++ src/io.cpp
@@ -129,21 +129,111 @@ bool send_param(const char * buffer, vector<Param> & vec_param)
 
 
 
-class params_handler
+class _params_handler
 {
 public:
-	params_handler();
-	~params_handler();
-	
+	_params_handler(ParamsOfLines * params);
+	virtual ~_params_handler();
+
 public:
+	void put(char ch);
+	void finishItem();
+	void finishLine();
 	
-	
+private:
+	ParamsOfLines * pol;
+	char buffer[1024];
+	char * ptr;
+	vector<Param> line;
 };
 
 
 
 
+class params_handler
+{
+public:
+	params_handler(ParamsOfLines * col_params, ParamsOfLines * row_params);
+	virtual ~params_handler();
+	
+public:
+	inline void put(char ch) {
+		_ph->put(ch);
+	}
+	inline void finishItem() {
+		_ph->finishItem();
+	}
+	inline void finishLine() {
+		_ph->finishLine();
+	}
+	inline void changeToRowMode() {
+		_ph = &_row;
+	}
+	
 
+private:
+	_params_handler _row;
+	_params_handler _col;
+	_params_handler * _ph;
+};
+
+
+
+params_handler::params_handler(ParamsOfLines * col_params, ParamsOfLines * row_params)
+: _row(row_params), _col(col_params)
+{
+	_ph = &_col;
+}
+
+
+params_handler::~params_handler()
+{
+}
+
+
+_params_handler::_params_handler(ParamsOfLines * param)
+{
+	pol = param;
+	ptr = buffer;
+}
+
+_params_handler::~_params_handler()
+{
+}
+
+void _params_handler::put(char ch)
+{
+	*ptr++ = ch;
+}
+
+
+void _params_handler::finishItem()
+{
+	*ptr = '\0';
+
+	Param param = create_param_from_str(buffer);
+	if (param.isValid())
+	{
+		line.push_back(param);
+	}
+	
+	ptr = buffer;
+}
+
+
+void _params_handler::finishLine()
+{
+	finishItem();
+	
+	if (line.size() > 0)
+	{
+		pol->push_back(line);
+		line.clear();
+	}
+}
+
+
+#define PH
 
 
 bool read_puzzle_file(const char * filename, ParamsOfLines & col_param, ParamsOfLines & row_param)
@@ -155,159 +245,39 @@ bool read_puzzle_file(const char * filename, ParamsOfLines & col_param, ParamsOf
 	}
 	
 	bool in_comment = false;
-	bool col_mode = true;
-	char buffer[1024];
-	char * p = buffer;
-	vector<Param> current;
+
+	params_handler ph1(&col_param, &row_param);
 	
-	while (!feof(file))
-	{
+	while (!feof(file)) {
 		char ch = (char)fgetc(file);
-		if (ch == '#' || ch == '%')
-		{
-			*p = '\0';
-			p = buffer;
-
-			send_param(buffer, current);
-
+		
+		if (ch == '#' || ch == '%') {
+			ph1.finishLine();
 			in_comment = true;
 		}
-		else if (ch == '\n' || ch == '\r')
-		{
-			*p = '\0';
-			p = buffer;
-			
-			send_param(buffer, current);
-			
-			if (current.size() > 0)
-			{
-				if (col_mode)
-				{
-					col_param.push_back(current);
-				}
-				else
-				{
-					row_param.push_back(current);
-				}
-				current.clear();
-			}
-
+		else if (ch == '\n' || ch == '\r') {
+			ph1.finishLine();
 			in_comment = false;
 		}
-		else if (!in_comment)
-		{
-			if (ch == '/' || ch == '+' || ch == '-')
-			{
-				*p = '\0';
-				p = buffer;
-				
-				send_param(buffer, current);
-				
-				if (current.size() > 0)
-				{
-					if (col_mode)
-					{
-						col_param.push_back(current);
-					}
-					else
-					{
-						row_param.push_back(current);
-					}
-					current.clear();
-				}
-				
-				col_mode = false;
-				
-				if (ch == '-')
-				{
+		else if (!in_comment) {
+			if (ch == '/' || ch == '+' || ch == '-') {
+				ph1.finishLine();
+				ph1.changeToRowMode();
+
+				if (ch == '-') {
 					in_comment = true;
 				}
 			}
-			else if (ch == ' ' || ch == '\t')
-			{
-				*p = '\0';
-				p = buffer;
-				
-				send_param(buffer, current);
+			else if (ch == ' ' || ch == '\t') {
+				ph1.finishItem();
 			}
-			else
-			{
-				*p++ = ch;
+			else if (ch != '\xFF'){
+				ph1.put(ch);
 			}
-			
 		}
-		
-		/*
-		switch (ch)
-		{
-			case '#':
-				in_comment = true;
-				break;
-				
-			case '\n':
-			case '\r':
-				*p = '\0';
-				send_param(buffer, current);
-				p = buffer;
-
-				if (current.size() > 0)
-				{
-					if (col_mode)
-					{
-						col_param.push_back(current);
-					}
-					else
-					{
-						row_param.push_back(current);
-					}
-					current.clear();
-				}
-				
-				in_comment = false;
-				break;
-				
-			case '/':
-				col_mode = false;
-				break;
-				
-				
-			default:
-				if (!in_comment)
-				{
-					if (ch == ' ' || ch == '\t')
-					{
-						*p = '\0';
-						send_param(buffer, current);
-						p = buffer;
-					}
-					else
-					{
-						*p++ = ch;
-					}
-				}
-				break;
-		}
-		 */
 	}
 	
-	*p = '\0';
-	p = buffer;
-	
-	send_param(buffer, current);
-	
-	if (current.size() > 0)
-	{
-		if (col_mode)
-		{
-			col_param.push_back(current);
-		}
-		else
-		{
-			row_param.push_back(current);
-		}
-		current.clear();
-	}
-	
+	ph1.finishLine();
 
 	
 	fclose(file);
@@ -316,7 +286,7 @@ bool read_puzzle_file(const char * filename, ParamsOfLines & col_param, ParamsOf
 }
 
 
-#if 0
+
 
 bool read_current_board_file(const char * filename, vector<pair<int, int>> & current_yes, vector<pair<int, int>> & current_no)
 {
@@ -327,7 +297,7 @@ bool read_current_board_file(const char * filename, vector<pair<int, int>> & cur
 	return true;
 }
 
-#endif
+
 
 
 
@@ -345,9 +315,7 @@ void output_param_of_lines(const ParamsOfLines & param_vec, FILE * output)
 	}
 }
 
-
-#if 0
-
+/*
 void output_board(const Board & board, FILE * output)
 {
 	for (int r = 0; r < board.getRowSize(); ++r)
@@ -359,5 +327,4 @@ void output_board(const Board & board, FILE * output)
 		fprintf(output, "\n");
 	}
 }
-
-#endif
+*/
